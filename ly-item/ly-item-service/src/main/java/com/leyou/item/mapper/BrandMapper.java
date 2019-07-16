c6b9129cacee967fa074591f9d1c39e4cbe9b950
ToonyQ123 <1918131749@qq.com>
2019-07-16 17:28:04 +0800

    Initial commit

---
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/mapper/BrandMapper.java
@@ -0,0 +1,12 @@
+package com.leyou.item.mapper;
+
+import com.leyou.item.pojo.Brand;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Component;
+import tk.mybatis.mapper.common.Mapper;
+@Component
+public interface BrandMapper extends Mapper<Brand> {
+    @Insert("INSERT INTO tb_category_brand(category_id,brand_id) VALUES (#{cid},#{bid})")
+    int insertCategoryBrand(@Param("cid") Long cid,@Param("bid") Long bid);
+}
